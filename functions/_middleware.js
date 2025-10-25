c802a3d6ce2afae07e09cc5e14503e2693bf47a7
XMZO <50775621+XMZO@users.noreply.github.com>
2025-10-25 21:19:48 +0800

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,18 +1,11 @@
 export async function onRequest(context) {
-  const { request, env } = context;
-  const url = new URL(request.url);
+  const { request } = context;
+  const hostname = request.headers.get("host");
   
-  // 自定义域名
-  const CUSTOM_DOMAIN = 'umi.li';
-  
-  // 检查是否是 pages.dev 域名
-  if (url.hostname.endsWith('.pages.dev')) {
-    // 构建新的 URL
-    const redirectUrl = new URL(url);
-    redirectUrl.hostname = CUSTOM_DOMAIN;
-    
-    // 使用 301 永久重定向
-    return Response.redirect(redirectUrl.toString(), 301);
+  // 只拦截 pages.dev，其他全部放行
+  if (hostname.endsWith('.pages.dev')) {
+    const url = new URL(request.url);
+    return Response.redirect(`https://umi.li${url.pathname}${url.search}`, 301);
   }
   
   return await context.next();
